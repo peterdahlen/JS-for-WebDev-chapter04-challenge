d9ff5575618697b0b7032ff759064197e3b9f4eb
Peter Dahlén <peterjandahlen@gmail.com>
2018-02-28 18:29:50 +0100

    2.0
    Svensk klocka och datum

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
 (function(){
-'use strict';
-
-document.addEventListener('DOMContentLoaded', function(){
-	var c = document.getElementById('current-time');
+	'use strict';
 	
-	setInterval(updateTime, 1000);
-	
-	function updateTime() {
-		var d = new Date();
-	
-		var hours = d.getHours();
-		var minutes = d.getMinutes();
-
+	document.addEventListener('DOMContentLoaded', function(){
+		var currentTime = document.getElementById('current-time'),
+			currentDate = document.getElementById('current-date');
+		
+		setInterval(function() {
+			var d = new Date();
+		
+			var hours = d.getHours(),
+				minutes = d.getMinutes(),
+				month = formatMonth(d.getMonth()),
+				date = d.getDate();
+			
 			if (minutes < 10) {
 				minutes = '0' + minutes;
 			}
+			
+			var sepClass = '';
+			if (d.getSeconds() % 2 === 1) sepClass = 'trans';
+			
+			var sep = '<span class="' + sepClass + '">:</span>';
 		
-		var sepClass = '';
-		if (d.getSeconds() % 2 === 1) sepClass = 'trans';
-
-		var sep = '<span class="' + sepClass + '">:</span>';
+			currentTime.innerHTML = hours + sep + minutes;
+			currentDate.textContent = date + ' ' + month;
+		}, 1000);
+		
+		function formatMonth(m) {
+			m = parseInt(m, 10);
+		
+			if (m < 0) {
+				m = 0;
+			} else if (m > 11) {
+				m = 11;
+			}
+		
+			var monthNames = [
+				'Januari', 'Februari', 'Mars',
+				'April', 'Maj', 'Juni', 
+				'Juli', 'Augusti', 'September',
+				'Oktober', 'November', 'December'
+			];
+			
+			return monthNames[m];
+		}
+	});
 	
-		c.innerHTML = hours + sep + minutes;
-	}
-});
-
-
-})();
+	
+	})();
